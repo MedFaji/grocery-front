b701eda3bd78a2ed8d6c539014d7e6ee3391f010
Med Faji <meedfaji@gmail.com>
2024-04-07 03:13:45 +0000

    Update Footer component to include top margin

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100">
+    <footer className="bg-gray-100 mt-[80px]">
       <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
         <div className="flex justify-center items-center text-teal-600">
           <Image src="/logo.png" alt="logo" width={50} height={20} />
